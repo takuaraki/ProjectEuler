d3a33ca8122dcb11be2e0c9a8a88f2411ab5bad1
araki taku <takuaraki7@gmail.com>
2014-02-25 00:14:49 +0900

    create40

---
+++ src/Problem40.java
@@ -0,0 +1,17 @@
+
+public class Problem40 {
+    
+    public static void main(String[] args) {
+
+        
+        
+    }
+
+    public static int d(int n){
+        return 0;
+    }
+    
+    public static int firstIndex(int n){
+        return 0;
+    }
+}
